95ab68af505cfee934909cf180560d08914f40a7
Eduardo Zepeda <69120108+eduardo-zepeda-j@users.noreply.github.com>
2021-08-27 10:23:22 -0600

    se agrega automata2.py

---
+++ automata2.py
@@ -0,0 +1,23 @@
+"""
+Identificador:
+secuencia de caracteres
+que inicia con una letra o guion
+bajo, seguida de n cantidad de letras,
+numeros o guiones bajos,
+
+pero si la cadena inicia con guiones bajos
+debera tener al menos una letra o numeros
+despues y n>=0
+
+
+a -> valido
+_esta_es_mi_variable -> valido
+_ -> no valido
+9_mi_variable -> no valido
+_a$ -> no valido
+_aq# -> no valido
+hola_mundo -> valido
+"""
+
+__a__a = 50
+print(__a__a)
